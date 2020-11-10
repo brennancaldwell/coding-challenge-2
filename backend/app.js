0177a13ccf1f03709da7b1fe44eacf255660ddd9
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-11-10 14:56:16 -0500

    Implement post route server-side

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const { TeamMember } = require('./model');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/team', async (req, res, next) => {
   const team = await TeamMember.findAll();
   return res.json(team);
 });
+
+app.post('/team', async (req, res, next) => {
+  console.log(req.body);
+  res.send('ok');
+})
 
 module.exports = app;
